d6df054ad03419d1607ce5f5e474387136d9127a
Christopher Neal <christopher.m.neal@gmail.com>
2022-02-03 14:18:33 -0800

    add tests, update exports
    Co-authored-by: Anna Pittman <annaepittman@gmail.com>
    Co-authored-by: Jeff Lai <pjefflai@gmail.com>
    Co-authored-by: Katie Pundt <kzpundt@gmail.com>

--- __tests__/haiku.test.js
+++ __tests__/haiku.test.js
@@ -1,9 +1,54 @@
-import { ExampleClass } from "../src/js/project.js";
+import {
+  haikuChecker,
+  wordSplitter,
+  lineSyllables,
+  syllableChecker,
+} from "../src/js/haiku.js";
 
-describe('ExampleClass', () => {
+// input examples:
+// ["the coffee is made", "the toast is in the toaster", "it is breakfast time"]
+// [["the", "coffee", "is", "made"], ["the", "toast", "is", "in", "the", "toaster"], ["it", "is", "breakfast", "time"]]
 
-  test('should correctly...', () => {
-    // const exampleClass = new ExampleClass();
-    // expect().toEqual();
+describe("wordSplitter", () => {
+  test("should correctly split line into own array", () => {
+    expect(wordSplitter(["the coffee is made"])).toStrictEqual([
+      ["the", "coffee", "is", "made"],
+    ]);
+  });
+
+  test("should trim extra spaces", () => {
+    expect(wordSplitter(["the coffee is made  "])).toStrictEqual([
+      ["the", "coffee", "is", "made"],
+    ]);
+  });
+});
+
+describe("haikuChecker", () => {
+  test("should identiy the number of syllabes", () => {
+    expect(haikuChecker(["the coffee is made", "yeah!"])).toStrictEqual("5, 1");
+  });
+});
+
+describe("lineSyllables", () => {
+  test("should add the number of syllables of each line", () => {
+    expect(lineSyllables(["the coffee is made"])).toStrictEqual(5);
+  });
+});
+
+describe("syllableChecker", () => {
+  test("should identify the number of syllables in a single word", () => {
+    expect(syllableChecker("the")).toStrictEqual(1);
+  });
+
+  test("should identify the number of syllables in a single word with an 'e' at the end", () => {
+    expect(syllableChecker("made")).toStrictEqual(1);
+  });
+
+  test("should identify the number of syllables in a single word with 'ea' in the word", () => {
+    expect(syllableChecker("neat")).toStrictEqual(1);
+  });
+
+  test("should identify the number of syllables in a single word", () => {
+    expect(syllableChecker("any")).toStrictEqual(2);
   });
 });
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import $ from 'jquery';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/styles.css';
+import $ from "jquery";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/styles.css";
 import { haikuChecker } from "./js/haiku.js";
 
-$(document).ready(function() {
-  $("#haiku").submit(function(event){
+$(document).ready(function () {
+  $("#haiku").submit(function (event) {
     event.preventDefault();
-    const text = $('#haiku-text').val().trim().split("\n");
+    const text = $("#haiku-text").val().trim().split("\n");
     const syllables = haikuChecker(text);
     const lines = text.length;
     const isHaiku = (syllables, lines) => {
@@ -17,7 +17,12 @@ $(document).ready(function() {
         return "Oopsie. Not a haiku :(";
       }
     };
-    $('#result').show();
-    $('#result').html(`<p>${isHaiku(syllables, lines)}<br>Syllable count: ${syllables} <br> Number of lines: ${lines}</p>`);
+    $("#result").show();
+    $("#result").html(
+      `<p>${isHaiku(
+        syllables,
+        lines
+      )}<br>Syllable count: ${syllables} <br> Number of lines: ${lines}</p>`
+    );
   });
 });
