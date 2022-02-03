c4a13e1e082fa896ad8ed5625a260dac62f60f10
Katie Pundt <kzpundt@gmail.com>
2022-02-02 16:06:23 -0800

    remove unneeded comments, start refactoring
    Co-authored-by: Christopher Neal <christopher.m.neal@gmail.com>
    Co-authored-by: Jeff Lai <pjefflai@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import { haikuChecker, lineChecker } from "./js/haiku.js";
+import { haikuChecker } from "./js/haiku.js";
 
 $(document).ready(function() {
   $("#haiku").submit(function(event){
     event.preventDefault();
-    const text = $('#haiku-text').val();
+    const text = $('#haiku-text').val().trim().split("\n");
     const syllables = haikuChecker(text);
-    const lines = lineChecker(text);
+    const lines = text.length;
     const isHaiku = (syllables, lines) => {
       if (syllables === "5, 7, 5" && lines === 3) {
         return "It's a haiku!";
--- src/js/haiku.js
+++ src/js/haiku.js
@@ -1,10 +1,4 @@
-export { haikuChecker, lineChecker };
-// const text = "the coffee is made \n the toast is in the toaster \n it is breakfast time"
-
-// returns an array of lines
-const lineSplitter = (text) => {
-  return text.trim().split("\n");
-};
+export { haikuChecker };
 
 // splits an array of lines into a nested array with individual words
 const wordSplitter = (line) => {
@@ -12,8 +6,7 @@ const wordSplitter = (line) => {
 };
 
 // returns array with syllable count of first three lines
-const haikuChecker = (text) => {
-  const lines = lineSplitter(text);
+const haikuChecker = (lines) => {
   const linesWords = wordSplitter(lines);
   const syllableCountArray = linesWords.map((lines) => lineSyllables(lines));
   return syllableCountArray.join(", ");
@@ -28,13 +21,6 @@ const lineSyllables = (wordsArray) => {
   return syllableSum;
 };
 
-// counts the number of lines
-const lineChecker = (text) => {
-  const lines = lineSplitter(text);
-  const linesLength = lines.length; 
-  return linesLength;
-};
-
 // counts the syllables of each word
 const syllableChecker = (word) => {
   // code borrowed from StackOverflow
@@ -45,62 +31,18 @@ const syllableChecker = (word) => {
   return word.match(/[aeiouy]{1,2}/g).length;
 
   // our code that only counts vowels right now
-  // const letterArray = word.toLowerCase().split("");
+
+  // trying to remove word final "e"s
+  // if (word[word.length - 1] === "e") {
+  //   const wordNoE = word.slice(0, word.length - 1);
+  // } else {
+  //   const wordNoE = word;
+  // }
+  
+  // this successfully counts vowels in each word
+  // const letterArray = wordNoE.toLowerCase().split("");
   // const vowelArray = letterArray.filter(e => e.match(/[aeiou]/g));
   // return vowelArray.length;
 };
 
 
-// //setup function
-// function findPrimes(input) {
-//   const array = Array.from({length: (input - 1)}, (_, i) => 2 + i);
-//   const primes = recurse(array);
-//   return `prime numbers less than ${input} are ${primes}`;
-// }
-
-// //recursive function
-// function recurse(array, index = 0) {
-//   if (index === array.length -1) {
-//     return array;
-//   }
-//   else {
-//     const filtered = array.filter(e => e % array[index] > 0 || e === array[index]);
-//     return recurse(filtered, index+1);
-//   }
-// }
-
-
-// const vowelCounter = (array, i, vowelCount) => {
-//   const vowelArray = ["a", "e", "i", "o", "u"];
-//   if (i >= array.length) {
-//     return vowelCount;
-//   } else if (vowelArray.includes(array[i])) {
-//     console.log(array[i]);
-//     return vowelCounter(array, i+1, vowelCount + 1)
-//     // return [array[i]].push(vowelCounter(array, i + 1));
-//   } else {
-//     return vowelCounter(array, i + 1, vowelCount);
-//   }
-// }
-
-// vowelCounter(['c', 'h', 'i', 'n'], 0, 0)
-
-// function new_count(word) {
-//   word = word.toLowerCase();
-//   if(word.length <= 3) { return 1; }
-//   word = word.replace(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '');
-//   word = word.replace(/^y/, '');
-//   return word.match(/[aeiouy]{1,2}/g).length;
-// }
-
-/*
-The "Written Method" Rules
-Count the number of vowels (A, E, I, O, U) in the word.
-Add 1 every time the letter 'y' makes the sound of a vowel (A, E, I, O, U).
-Subtract 1 for each silent vowel (like the silent 'e' at the end of a word).
-Subtract 1 for each diphthong or triphthong in the word.
-  Diphthong: when 2 vowels make only 1 sound (au, oy, oo)
-  Triphthong: when 3 vowels make only 1 sound (iou)
-Does the word end with "le" or "les?" Add 1 only if the letter before the "le" is a consonant.
-The number you get is the number of syllables in your word.
-*/
